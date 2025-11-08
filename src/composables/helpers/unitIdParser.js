693b38a75332b5064a5dc416d14d4f4df3ec88c4
Kirill Riman <55841348+K-ETFreeman@users.noreply.github.com>
2025-11-08 21:32:37 +0300

    sort fix

--- src/composables/helpers/unitIdParser.js
+++ src/composables/helpers/unitIdParser.js
@@ -1,3 +1,3 @@
-export const getUnitNumber = (id) => id.slice(-4)
+export const getUnitNumber = (id) => (id.slice(-4).match(/\d+$/)?.[0] || 0) - 0
 
 export const getFactionCode = (id) => id.slice(0, 3)
